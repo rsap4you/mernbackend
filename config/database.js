cc62162d584f7734f8d544159ad58a62a4cbf5dd
rsap4you <kumaramarjeetraj71@gmail.com>
2024-03-13 14:10:01 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const mongoose =  require('mongoose');
 
 // const dbUrl = process.env.APP_ENV === "development" ? process.env.DEV_DB_URL : process.env.LIVE_DB_URL;
-const dbUrl = process.env.APP_ENV === "development" ? process.env.DEV_DB_URL : process.env.DATABASE;
+// const dbUrl = process.env.APP_ENV === "development" ? process.env.DEV_DB_URL : process.env.DATABASE;
+const dbUrl =process.env.DATABASE
 
 
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
