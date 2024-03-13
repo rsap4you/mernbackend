9552551e2a2dabdd464cfb895be5e2065ac243e4
rsap4you <kumaramarjeetraj71@gmail.com>
2024-03-13 13:16:33 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose =  require('mongoose');
 
 // const dbUrl = process.env.APP_ENV === "development" ? process.env.DEV_DB_URL : process.env.LIVE_DB_URL;
-const dbUrl = process.env.APP_ENV === "development" ? process.env.DEV_DB_URL : process.env.DATABASE;
+const dbUrl = process.env.APP_ENV === "development" ? process.env.DEV_DB_URL : 'mongodb+srv://kumaramarjeetraj7725:fennn5rlpwlNF8pO@cluster0.mdlovbh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
