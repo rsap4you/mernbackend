69cd6fc5014d0fae2255033a82bbaabbb07d0b5e
unknown <kumaramarjeetraj7725@gmail.com>
2025-01-24 12:40:01 +0530

    Initial commit

--- modules/v1/user/user_validation_rules.js
+++ modules/v1/user/user_validation_rules.js
@@ -1,11 +1,11 @@
 const checkValidatorRules = {
 
     sigupValidation: {
-        
+
         // first_name: "required",
         // last_name: "required",
          // dob: "required",
-        mobile_number: "required|digits_between:10,14",
+        //  mobile_number: "required|digits_between:10,14",
         // email: "required|email",
         // password: "required",
         // token: "required",
@@ -13,17 +13,17 @@ const checkValidatorRules = {
     },
 
     otpValidation: {
-        email: "required",
-        otp: "required"
+        // email: "required",
+        // otp: "required"
     },
 
     resendotpValidation: {
-        mobile: "required"
+        // mobile: "required"
     },
 
     loginValidation: {
-        email: "required",
-        password: "required"
+        // email: "required",
+        // password: "required"
     },
 
     updatePasswordValidation: {
