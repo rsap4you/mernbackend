652b0500d69f45e5d11b89e400d57e04e0991f7d
unknown <kumaramarjeetraj7725@gmail.com>
2025-03-15 10:55:48 +0530

    Initial commit

--- modules/v1/user/models/user_model.js
+++ modules/v1/user/models/user_model.js
@@ -454,7 +454,7 @@ const userModel = {
           return await middleware.sendResponse(res, Codes.NOT_FOUND, 'User not found', null);
       }
 
-    if(Number(req.points) === 100){
+    // if(Number(req.points) === 100){
         const newRedeem = new RedeemSchema({
             user_id:req.user_id,
             full_name :userExists.full_name,
@@ -479,10 +479,10 @@ const userModel = {
         });
 
         return await middleware.sendResponse(res, Codes.SUCCESS, 'Success', redeemdetails);
-    }else{
-        console.error("Error in Redeem function  2000000:")
-        return await middleware.sendResponse(res, Codes.NOT_FOUND, 'You are not eligible for withrawal for rupees.', null);
-    }
+    // }else{
+    //     console.error("Error in Redeem function  2000000:")
+    //     return await middleware.sendResponse(res, Codes.NOT_FOUND, 'You are not eligible for withrawal for rupees.', null);
+    // }
 
 
         } catch (error) {
